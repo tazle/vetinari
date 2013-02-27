5fb5d842c455178bb18d26fae8f16a92c6977bea
Tuure Laurinolli <tuure@laurinolli.net>
2013-02-27 23:20:26 +0200

    Try to show something

--- clock.js
+++ clock.js
@@ -1,10 +1,11 @@
 var app = {
 
     initialize: function() {
-        var canvas = $('clock');
+        var canvas = $("clock");
         var ctx = canvas.getContext('2d');
         ctx.fillStyle = "rgb(200, 0, 0)";
         ctx.fillRect(10, 10, 50, 50);
+        $("afterDraw").show();
     }
 
 };
