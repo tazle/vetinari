84ec40a5c6c414355e82ae6f2a023248256987b9
Tuure Laurinolli <tuure@laurinolli.net>
2013-02-27 23:13:45 +0200

    Initial test

---
+++ clock.js
@@ -0,0 +1,14 @@
+var app = {
+
+    initialize: function() {
+        var canvas = $('clock');
+        var ctx = canvas.getContext('2d');
+        ctx.fillStyle = "rgb(200, 0, 0)";
+        ctx.fillRect(10, 10, 50, 50);
+    }
+
+};
+
+function onLoad() {
+    document.addEventListener("deviceready", onDeviceReady, false);
+}
